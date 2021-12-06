752043f092b35bdd96479da38522de3e9bce794a
HwiChanYu <dbgnlcks34@gmail.com>
2021-12-06 10:06:58 +0900

    로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,11 +1,40 @@
 "use strict";
 
-const home = (req, res) => {
-  res.render("home/index");
+const users = {
+  id: ["dbgnlcks", "star", "idid"],
+  pw: ["1234", "560323", "3434"]
 };
 
-const login = (req, res) => {
-  res.render("home/login");
-}
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  }
+};
 
-module.exports = { home, login };
+const process = {
+  login: (req, res) => {
+    const { id, pw } = req.body;
+    if(users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if(users.pw[idx] === pw) {
+        return res.json({ success: true });
+      }
+
+      return res.json({
+        success: false,
+        msg: "비밀번호가 틀렸습니다."
+      })
+    }
+    
+   return res.json({
+      success: false,
+      msg: "존재하지 않는 아이디입니다."
+    })
+  }
+};
+
+
+module.exports = { output, process };
